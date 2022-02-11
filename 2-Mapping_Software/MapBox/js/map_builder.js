5e181285eaab924687d3a54d32f8abf002f20649
Alex Canales <alexcanales766@gmail.com>
2022-02-11 00:50:08 -0500

    created sliders

--- 2-Mapping_Software/MapBox/js/map_builder.js
+++ 2-Mapping_Software/MapBox/js/map_builder.js
@@ -2,6 +2,31 @@
 Date: 2022-02-09
 */
 
+
+let open_modal_btn;
+let modal;
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+    open_modal_btn = document.getElementById("open-modal-btn");
+    open_modal_btn.addEventListener("click", function () {
+        modal.style.display = 'block';
+       
+    });
+
+    close_modal_btn = document.getElementById("close-modal-btn");
+    close_modal_btn.addEventListener("click", function () {
+
+      modal.style.display = 'none';
+    });
+
+    modal = document.getElementsByClassName("modal")[0];
+
+    //download_btn = document.getElementById("download-btn");
+    //download_btn.addEventListener("click", download_all);
+  });
+  
+
 //Access token to load Mapbox into project
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FuYWxlYWwiLCJhIjoiY2t6Nmg2Z2R4MTBtcDJ2cW9xMXI2d2hqYyJ9.ef3NOXxDnIy4WawQuaFopg';
 
@@ -25,6 +50,7 @@ const map = new mapboxgl.Map({
 });
 
 
+
 //Add the mapbox map controls like fullscreen mode and navigation search bar
 function add_map_controls() {
     //Geocoder is used to specify the langauge. Examples include EN for english, ES for spanish
@@ -43,12 +69,38 @@ function add_map_controls() {
 }
 
 
+
+var toggleableLayerIds = [];
+
+function add_terrian_layer(){
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+         
+        // add a sky layer that will show when the map is highly pitched
+        map.addLayer({
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+        'sky-type': 'atmosphere',
+        'sky-atmosphere-sun': [0.0, 0.0],
+        'sky-atmosphere-sun-intensity': 15
+        }
+        });
+}
+
+
 //This function adds the Kingston city bounderies
 function add_boundry_layer() {
     //Add the city bounderies layer
     map.addSource('Boundry', {
         'type': 'geojson',
-        'data': 'http://127.0.0.1:5500/data/neighbourhoods.geojson',
+        'data': 'http://127.0.0.1:5500/data/city-neighbourhoods.geojson',
     });
 
     // Add a new layer to visualize the bounderies.
@@ -58,7 +110,7 @@ function add_boundry_layer() {
         'source': 'Boundry', // reference the data source
         'layout': {},
         'paint': {
-            'fill-color': '#0078d3', // blue color fill
+            'fill-color': ['get', 'fill'], // blue color fill
             'fill-opacity': 0.3 //Set opacity of the polygon
         }
     });
@@ -73,6 +125,34 @@ function add_boundry_layer() {
             'line-width': 1
         }
     });
+
+
+    map.addLayer({
+        'id': 'BoundryIcon',
+        'type': 'symbol',
+        'source': 'Boundry',
+        'layout': {
+            'icon-image': 'marker',
+            // get the title name from the source's "title" property
+            'text-field': ['get', 'name'],
+            'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+            ],
+           
+
+            'text-offset': [0, 1.25],
+            'text-anchor': 'top'
+        },
+        'paint': {
+            "text-color": "#ffffff"
+
+        }
+
+    });
+
+    toggleableLayerIds.push('Boundry')
+    map.setLayoutProperty('Boundry', 'visibility', 'visible');
 }
 
 
@@ -88,7 +168,7 @@ function add_building_layer() {
     // from OpenStreetMap.
     map.addLayer(
         {
-            'id': 'add-3d-buildings',
+            'id': '3D_Buildings',
             'source': 'composite',
             'source-layer': 'building',
             'filter': ['==', 'extrude', 'true'],
@@ -123,10 +203,13 @@ function add_building_layer() {
         },
         labelLayerId
     );
+
+    toggleableLayerIds.push('3D_Buildings')
+    map.setLayoutProperty('3D_Buildings', 'visibility', 'visible');
 }
 
 
-function add_road_layer() {
+function add_capital_planning_lines_layer() {
     //Create the roads under development layer
     map.addSource('Roads', {
         'type': 'geojson',
@@ -145,6 +228,9 @@ function add_road_layer() {
             'line-width': 3 //Specify width of the road
         }
     });
+
+    toggleableLayerIds.push('Roads')
+    map.setLayoutProperty('Roads', 'visibility', 'visible');
 
 }
 
@@ -182,9 +268,128 @@ function add_development_layer() {
                 }
 
             });
+
+            
+            map.setLayoutProperty('Developments', 'visibility', 'visible');
         }
+        
     )
 
+    toggleableLayerIds.push('Developments')
+}
+
+
+function add_road_surface_layer(){
+    //Create the roads under development layer
+    map.addSource('RoadsSurface', {
+        'type': 'geojson',
+        'data': 'http://127.0.0.1:5500/data/road-surface.geojson'
+    });
+    map.addLayer({
+        'id': 'RoadsSurface',
+        'type': 'line',
+        'source': 'RoadsSurface',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#888888', //Specify road colour
+            'line-width': 3 //Specify width of the road
+        }
+    });
+
+    toggleableLayerIds.push('RoadsSurface')
+    map.setLayoutProperty('RoadsSurface', 'visibility', 'visible');
+}
+
+
+function add_tree_layer(){
+     //Add the city bounderies layer
+     map.addSource('Tree', {
+        'type': 'geojson',
+        'data': 'http://127.0.0.1:5500/data/tree-groups.geojson',
+    });
+
+    // Add a new layer to visualize the bounderies.
+    map.addLayer({
+        'id': 'Tree',
+        'type': 'fill',
+        'source': 'Tree', // reference the data source
+        'layout': {},
+        'paint': {
+            'fill-color': '#3d552c', // blue color fill
+            'fill-opacity': 0.3 //Set opacity of the polygon
+        }
+    });
+    // Add a black outline around the polygon.
+    map.addLayer({
+        'id': 'TreeOutline',
+        'type': 'line',
+        'source': 'Tree',
+        'layout': {},
+        'paint': {
+            'line-color': '#009900',
+            'line-width': 1
+        }
+    });
+
+
+    toggleableLayerIds.push('Tree')
+    map.setLayoutProperty('Tree', 'visibility', 'visible');
+}
+
+
+
+
+function add_pedestrian_layer(){
+    //Add the city bounderies layer
+    map.addSource('Pedestrians', {
+       'type': 'geojson',
+       'data': 'http://127.0.0.1:5500/data/pedestriancrossings-fakedata.geojson',
+   });
+
+   // Add a new layer to visualize the bounderies.
+   map.addLayer({
+       'id': 'Pedestrians',
+       'type': 'fill',
+       'source': 'Pedestrians', // reference the data source
+       'layout': {},
+       'paint': {
+           'fill-color': '#ff0000', // blue color fill
+           'fill-opacity': 0.8 //Set opacity of the polygon
+       }
+   });
+
+
+  
+    map.addLayer({
+        'id': 'PedestriansIcon',
+        'type': 'symbol',
+        'source': 'Pedestrians',
+        'layout': {
+            'icon-image': 'marker',
+            // get the title name from the source's "title" property
+            'text-field': ['get', 'name'],
+            'text-font': [
+                'Open Sans Semibold',
+                'Arial Unicode MS Bold'
+            ],
+
+
+            'text-offset': [0, 1.25],
+            'text-anchor': 'top'
+        },
+        'paint': {
+            "text-color": "#ffffff"
+
+        }
+
+    });
+   
+
+   toggleableLayerIds.push('Pedestrians')
+   map.setLayoutProperty('Pedestrians', 'visibility', 'visible');
 }
 
 function add_toggle_for_developments() {
@@ -229,7 +434,7 @@ function add_toggle_for_developments() {
 
 }
 
-function add_toggle_for_roads() {
+function add_toggle_for_capital_planning_lines() {
     map.on('click', 'Roads', (e) => {
         // Copy coordinates array.
         var coord = e.features[0].properties.geo_point_2d;
@@ -270,6 +475,8 @@ function add_toggle_for_roads() {
 }
 
 
+
+
 function add_toggle_for_boundry() {
     map.on('click', 'Boundry', (e) => {
         // Copy coordinates array.
@@ -294,10 +501,32 @@ function add_toggle_for_boundry() {
     });
 }
 
+
+function add_toggle_for_predestrian() {
+    map.on('click', 'Pedestrians', (e) => {
+        // Copy coordinates array.
+        var coord = e.features[0].geometry.coordinates[0][0];
+        console.log(coord)
+        const coordinates = coord;
+        const description = `<h3>${e.features[0].properties.name}</h3>`
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+}
+
 function add_menu_to_select_layers(){
     //The different layers are saved here
-    var toggleableLayerIds = ['Developments', 'Roads', 'Boundry', 'outline'];
-
+   
     for (var i = 0; i < toggleableLayerIds.length; i++) {
       var id = toggleableLayerIds[i];
 
@@ -312,7 +541,7 @@ function add_menu_to_select_layers(){
         e.stopPropagation();
 
         var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
+        console.log(visibility)
         if (visibility === 'visible') {
           map.setLayoutProperty(clickedLayer, 'visibility', 'none');
           this.className = '';
@@ -328,21 +557,39 @@ function add_menu_to_select_layers(){
 }
 
 
+
+function add_layers(){
+    //Layers
+    add_terrian_layer()
+    add_boundry_layer()
+    add_building_layer()
+    //add_capital_planning_lines_layer()
+    add_development_layer()
+    //add_road_surface_layer()
+    //add_tree_layer()
+    add_pedestrian_layer()
+
+
+    //Allow popups to show up when layer is clicked
+    //add_toggle_for_boundry()
+    //add_toggle_for_capital_planning_lines()
+    add_toggle_for_developments()
+    add_toggle_for_predestrian()
+}
+
+
 map.on('load', () => {
     //Controls
     add_map_controls()
 
-    //Layers
-    add_boundry_layer()
-    add_building_layer()
-    add_road_layer()
-    add_development_layer()
-
-    //Allow popups to show up when layer is clicked
-    add_toggle_for_boundry()
-    add_toggle_for_roads()
-    add_toggle_for_developments()
+    
+    add_layers()
 
     //Add the menu to make layers visible or invisible
     add_menu_to_select_layers()
+   
 });
+
+
+
+
