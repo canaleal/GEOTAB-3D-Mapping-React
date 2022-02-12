2a8bee54928b9559e39c92d648f59a63c17cbf2b
Amir Afshari <a_amirhossein@icloud.com>
2022-02-12 16:36:37 -0500

    initial commit contains redux libraries and tailwind

---
+++ 2-Mapping_Software/mapbox-react-demo/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ">
+      <p className='text-green-400'>
+        ssd
+      </p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
