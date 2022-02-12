3130544769cfa51d99b43760ebd0ebd65b4850c0
Amir Afshari <a_amirhossein@icloud.com>
2022-02-12 17:00:29 -0500

    adds mapbox to app.js

--- 2-Mapping_Software/mapbox-react-demo/src/App.js
+++ 2-Mapping_Software/mapbox-react-demo/src/App.js
@@ -1,15 +1,39 @@
+import "./App.css";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { useRef, useState, useEffect } from "react";
 
-import './App.css';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2FuYWxlYWwiLCJhIjoiY2t6Nmg2Z2R4MTBtcDJ2cW9xMXI2d2hqYyJ9.ef3NOXxDnIy4WawQuaFopg";
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+     // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
-    <div className="App ">
-      <p className='text-green-400'>
-        ssd
-      </p>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <div ref={mapContainer} className="map-container h-screen"/>
     </div>
   );
 }
