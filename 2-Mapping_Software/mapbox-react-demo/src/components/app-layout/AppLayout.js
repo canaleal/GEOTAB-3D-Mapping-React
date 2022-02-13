22d53e500563b3b7c8a1f3a91fefc87781c2486d
Amir Afshari <a_amirhossein@icloud.com>
2022-02-12 20:50:26 -0500

    adds civilian type filter to sidebar

--- 2-Mapping_Software/mapbox-react-demo/src/components/app-layout/AppLayout.js
+++ 2-Mapping_Software/mapbox-react-demo/src/components/app-layout/AppLayout.js
@@ -1,56 +1,51 @@
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-} from "@ant-design/icons";
+import { Layout, Menu, Breadcrumb, Radio } from "antd";
+import { GiWalk } from "react-icons/gi";
 import Map from "../map/Map";
+import { useState } from "react";
+import { MdDirectionsBike } from "react-icons/md";
+import { IoIosCar } from "react-icons/io";
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 const AppLayout = () => {
+  const [civFilter, setCivFilter] = useState();
+
+  const handleCivFilterChange = (e) => {
+    setCivFilter(e.target.value);
+  };
+
   return (
     <Layout className="h-screen">
       <Header className="">
         {/* <div className="logo" /> */}
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu.Item key="1">Civilians</Menu.Item>
+          <Menu.Item key="2">Road Conditions</Menu.Item>
         </Menu>
       </Header>
       <Layout>
-        <Sider width={200} className="">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            className="h-full"
-          >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub3"
-              icon={<NotificationOutlined />}
-              title="subnav 3"
+        <Sider width={300} className="p-2" theme="light">
+          <div>
+            <Radio.Group
+              optionType="button"
+              buttonStyle="solid"
+              value={civFilter}
+              onChange={handleCivFilterChange}
             >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
+              <Radio.Button value="pedestrian">
+                <GiWalk className="inline" />
+              </Radio.Button>
+              <Radio.Button value="bike">
+                <MdDirectionsBike className="inline" />
+              </Radio.Button>
+              <Radio.Button value="traffic">
+                <IoIosCar className="inline" />
+              </Radio.Button>
+              <Radio.Button value="all">All</Radio.Button>
+            </Radio.Group>
+            <Radio.Group optionType="button" buttonStyle="solid"></Radio.Group>
+          </div>
         </Sider>
         <Layout className="pl-5 pt-5">
           <Content className="">
