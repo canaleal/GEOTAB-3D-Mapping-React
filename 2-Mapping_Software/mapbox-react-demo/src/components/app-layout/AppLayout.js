ecf8fa14b127f0c295d7b7636e193d20eb0e13a8
Amir Afshari <a_amirhossein@icloud.com>
2022-02-12 22:07:48 -0500

    adds styling to sider

--- 2-Mapping_Software/mapbox-react-demo/src/components/app-layout/AppLayout.js
+++ 2-Mapping_Software/mapbox-react-demo/src/components/app-layout/AppLayout.js
@@ -20,16 +20,11 @@ const AppLayout = () => {
     setPedestrianFilter(e.target.value);
   };
 
-  const pedestrianOptions = [
-    { label: "Crossing", value: "Crossing" },
-    { label: "Jaywalking", value: "Jaywalking" },
-    { label: "All", value: "All" },
-  ];
+  const pedestrianOptions = [];
   return (
     <Layout className="h-screen">
       <Header className="">
-        <div>
-        </div>
+        <div></div>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1">Civilians</Menu.Item>
           <Menu.Item key="2">Road Conditions</Menu.Item>
@@ -43,34 +38,46 @@ const AppLayout = () => {
               buttonStyle="solid"
               value={civFilter}
               onChange={handleCivFilterChange}
+              style={{ display: "flex" }}
             >
-              <Radio.Button value="pedestrian">
+              <Radio.Button value="pedestrian" className="grow text-center">
                 <GiWalk className="inline" />
               </Radio.Button>
-              <Radio.Button value="bike">
+              <Radio.Button value="bike" className="grow text-center">
                 <MdDirectionsBike className="inline" />
               </Radio.Button>
-              <Radio.Button value="traffic">
+              <Radio.Button value="traffic" className="grow text-center">
                 <IoIosCar className="inline" />
               </Radio.Button>
-              <Radio.Button value="all">All</Radio.Button>
+              <Radio.Button value="all" className="grow text-center">
+                All
+              </Radio.Button>
             </Radio.Group>
-            <Radio.Group optionType="button" buttonStyle="solid"></Radio.Group>
-            
-            <Radio.Group
-            defaultValue={"All"}
-              options={pedestrianOptions}
-              optionType="button"
-              buttonStyle="solid"
-              value={pedestrianFilter}
-              onChange={handlePedestrianFilterChange}
-            ></Radio.Group>
+
+            <div className="mt-2">
+              <Radio.Group
+                defaultValue={"All"}
+                buttonStyle="solid"
+                value={pedestrianFilter}
+                onChange={handlePedestrianFilterChange}
+                style={{ display: "flex" }}
+              >
+                <Radio.Button value="Crossing" className="grow text-center">
+                  Crossing
+                </Radio.Button>
+                <Radio.Button value="Jaywalking" className="grow text-center">
+                  Jaywalking
+                </Radio.Button>
+                <Radio.Button value="All" className="grow text-center">
+                  All
+                </Radio.Button>
+              </Radio.Group>
+            </div>
           </div>
 
           <div className="border-2 mt-2 rounded-md p-3">
-            <img src={require("../../images/pedestrian.png")}></img>
+            <img alt="chart" src={require("../../images/pedestrian.png")}></img>
           </div>
-
         </Sider>
         <Layout className="pl-5 pt-5">
           <Content className="">
