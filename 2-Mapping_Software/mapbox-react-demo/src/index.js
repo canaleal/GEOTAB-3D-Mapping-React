02825e6a7c6a40751622f23b7125d895919bf296
Alex Canales <alexcanales766@gmail.com>
2022-03-14 00:12:34 -0400

    Index js router and font awesome added

--- 2-Mapping_Software/mapbox-react-demo/src/index.js
+++ 2-Mapping_Software/mapbox-react-demo/src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+
+import Home from "./pages/home/Home.js";
+import Error from "./pages/error/Error.js";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+//Import font awesome and some icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLayerGroup, faX, faPlus, faBuilding, faBorderAll, faPerson, faBus, faBuildingColumns, faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+
+//Add the icons to the library so they can be used in ever single component
+library.add( faLayerGroup, faX, faPlus, faBuilding, faBorderAll, faPerson, faBus, faBuildingColumns, faPlay, faPause)
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
