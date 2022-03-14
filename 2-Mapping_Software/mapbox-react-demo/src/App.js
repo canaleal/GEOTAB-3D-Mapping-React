b10da8addb95a692cf4af6ba659cddf5cee0a23a
Alex Canales <alexcanales766@gmail.com>
2022-03-14 00:13:16 -0400

    App.js router added

--- 2-Mapping_Software/mapbox-react-demo/src/App.js
+++ 2-Mapping_Software/mapbox-react-demo/src/App.js
@@ -1,14 +1,37 @@
-import "./App.css";
-import AppLayout from "./components/app-layout/AppLayout";
+import logo from './logo.svg';
+import { Outlet, Link } from "react-router-dom";
+import './App.css';
+
+import HelpModal from './components/HelpModal';
+
+import { useState, useRef, Fragment } from 'react';
+import Footer from './components/Footer';
+
 
 
 function App() {
+
   
 
   return (
-    <div>
-      <AppLayout></AppLayout>
-    </div>
+    <Fragment>
+
+    
+      <nav className="bg-blue-500 color-white">
+        <div className='px-5 py-5  '>
+          <Link to="/" className="nav-item">
+            <span className='text-xl weight-bold'>A.I.M 3D Mapping</span>
+          </Link>   
+        </div>
+      </nav>
+
+    
+      
+      
+      <Outlet />
+
+      
+    </Fragment>
   );
 }
 
