20461b889fcb7af6f467b3ac1ca51863280cbc23
Alex Canales <alexcanales766@gmail.com>
2022-03-30 21:05:03 -0400

    Added ability to update streetview location without having to create a NEW streetview panorama

--- 2-Mapping_Software/mapbox-react-demo/src/pages/home/components/googleStreetview/GoogleStreetview.js
+++ 2-Mapping_Software/mapbox-react-demo/src/pages/home/components/googleStreetview/GoogleStreetview.js
@@ -8,49 +8,54 @@ const GoogleStreetview = (pointOfInterest) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   const streetviewRef = useRef();
+  const [streetViewPanorama, setStreetViewPanorama] = useState();
 
 
   useEffect(() => {
 
-    try{
-     
-      let coordinates = [];
-      if(pointOfInterest.pointOfInterest._geometry.type == 'LineString'){
-        // If the point of interest is a line, get the coordinates of the first point
-         coordinates = pointOfInterest.pointOfInterest._geometry.coordinates[0];
+    const location_coordinates = getLocationCoordinates(pointOfInterest);
+    // Create the StreetViewPanorama and set it to the div element
+    setStreetViewPanorama(new google.maps.StreetViewPanorama(
+      streetviewRef.current, {
+      position: location_coordinates,
+      pov: {
+        heading: 34,
+        pitch: 10
       }
-      else{
-        // If the point of interest is a point, get the coordinates of the point
-         coordinates = pointOfInterest.pointOfInterest._geometry.coordinates;
-      }
+    }));
 
-      // Set the streetview location coordinates using Point of Interest coordinates
-      const location_coordinates = { lat: coordinates[1], lng: coordinates[0] };
-      
+  }, [])
 
-      // Create a new StreetViewService object using the Point of Interest coordinates
-      new google.maps.StreetViewPanorama(
-        streetviewRef.current, {
-        position: location_coordinates,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      });
 
-      setIsLoaded(true);
-      setError(false);
-    }
-    catch (Exception){
-     
-      setIsLoaded(true);
-      setError(true);
-    }
+  useEffect(() => {
+
+
+    const location_coordinates = getLocationCoordinates(pointOfInterest);
+    streetViewPanorama.setPosition(location_coordinates);
+
 
   }, [pointOfInterest])
 
+
+  function getLocationCoordinates(pointOfInterest) {
+    let coordinates = [];
+    if (pointOfInterest.pointOfInterest._geometry.type == 'LineString') {
+      // If the point of interest is a line, get the coordinates of the first point
+      coordinates = pointOfInterest.pointOfInterest._geometry.coordinates[0];
+    }
+    else {
+      // If the point of interest is a point, get the coordinates of the point
+      coordinates = pointOfInterest.pointOfInterest._geometry.coordinates;
+    }
+
+    // Set the streetview location coordinates using Point of Interest coordinates
+    const location_coordinates = { lat: coordinates[1], lng: coordinates[0] };
+
+    return location_coordinates;
+  }
+
   return (
-    
+
 
     <div ref={streetviewRef} className='h-96 md:h-128 rounded-lg'></div>
   )
