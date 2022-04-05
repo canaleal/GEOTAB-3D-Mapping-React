e2344e3d1338642ecff4b0c821ea62d249ba917f
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2022-04-05 15:18:57 -0400

    adds legend component

---
+++ 2-Mapping_Software/mapbox-react-demo/src/pages/home/components/map/GradientLegend.js
@@ -0,0 +1,11 @@
+const GradientLegend = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <span>1000</span>
+      <div className="h-28 w-3 bg-gradient-to-b from-[#900b0a] to-[#74ed86] rounded-lg" />
+      <span>0</span>
+    </div>
+  );
+};
+
+export default GradientLegend;
