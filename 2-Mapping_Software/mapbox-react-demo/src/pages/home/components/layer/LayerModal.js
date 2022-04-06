c633ff0348a82df48929cd17ab623aa8456a3aec
Alex Canales <alexcanales766@gmail.com>
2022-04-05 21:14:09 -0400

    Simplified Layer modal

--- 2-Mapping_Software/mapbox-react-demo/src/pages/home/components/layer/LayerModal.js
+++ 2-Mapping_Software/mapbox-react-demo/src/pages/home/components/layer/LayerModal.js
@@ -1,17 +1,19 @@
 
 
-import React  from 'react'
+import React from 'react'
 import ModalListItem from './ModalListItem'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const LayerModal = ({ layerModalRef, closeModalHandler, staticLayers, dynamicLayers, layerButtonHandler }) => {
+const LayerModal = ({ layerModalRef, closeModalHandler, layers, layerButtonHandler }) => {
+
 
 
 
     return (
         <div ref={layerModalRef} className='popup-box flex flex-col rounded-lg p-5'>
-            <div className='grid grid-cols-1  md:grid-cols-4  gap-4'>
 
+
+            <div className='grid grid-cols-1  md:grid-cols-4  gap-4'>
                 <div className='cols-span-1'>
                     <button onClick={() => closeModalHandler(layerModalRef)} className='border   w-full  rounded-md text-center p-2 btn-gray'>
                         <FontAwesomeIcon icon={`fa-solid fa-x`} />
@@ -20,38 +22,17 @@ const LayerModal = ({ layerModalRef, closeModalHandler, staticLayers, dynamicLay
             </div>
 
 
-            {staticLayers.length > 0 ?
-                <>
-                    <hr className='my-5'></hr>
-                    <p className="text-xl font-bold">Static</p>
-                    <div className='grid grid-cols-1 md:grid-cols-4  gap-4'>
-                        {staticLayers.map((layer) => {
-                            return (
-                                <ModalListItem key={layer.id} layer={layer} layerButtonHandler={layerButtonHandler} />
-                            )
-                        })}
-                    </div>
-                </>
-                :
-                <></>
-            }
 
-            {dynamicLayers.length > 0 ?
-                <>
-                    <hr className='my-5'></hr>
-                    <p className="text-xl font-bold">Dynamic</p>
-                    <div className='grid grid-cols-1 md:grid-cols-4  gap-4'>
-                        {dynamicLayers.map((layer) => {
-                            return (
-                                <ModalListItem key={layer.id} layer={layer} layerButtonHandler={layerButtonHandler} />
-                            )
-                        })}
-                    </div>
-                </>
-                :
-                <></>
-            }
 
+            <hr className='my-5'></hr>
+
+            <div className='grid grid-cols-1 md:grid-cols-4  gap-4'>
+                {layers.map((layer) => {
+                    return (
+                        <ModalListItem key={layer.id} layer={layer} layerButtonHandler={layerButtonHandler} />
+                    )
+                })}
+            </div>
 
         </div>
 
